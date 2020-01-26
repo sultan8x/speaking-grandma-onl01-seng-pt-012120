518b1c4f79687d7e385fd31e820f8f8cecfeea53
Sultan Afsheen <leadsultan8x@gmail.com>
2020-01-26 21:47:27 +0000

    Done.

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,16 @@
-require_relative '../grandma.rb'
+require 'pry'
 
+def speak_to_grandma(phrase)
 
+  if phrase == "I LOVE YOU GRANDMA!"
+    return "I LOVE YOU TOO PUMPKIN!"
+
+  elsif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+
+  elsif phrase == phrase.upcase
+    return "NO, NOT SINCE 1938!"
+
+  end
+
+  end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,12 +3,12 @@
 # Require this file using `require "spec_helper"` to ensure that it is only
 # loaded once.
 
-require_relative grandma.rb
+require_relative '../grandma.rb'
 
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 
 RSpec.configure do |config|
-  
+
   # config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
   config.filter_run :focus
@@ -17,6 +17,6 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  
+
   config.order = 'default'
 end
